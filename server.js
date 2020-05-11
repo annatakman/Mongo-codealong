f52f1407fd181766c004335972e1753408764b93
annatakman <annatakman@gmail.com>
2020-05-11 21:45:10 +0200

    started codealong, error message, app crashed

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Author = mongoose.model('Author', {
+  name: String
+})
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.Objectid,
+    ref: 'Author'
+  }
+})
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async = () => {
+    await Author.deleteMany()
+    const tolkien = new Author({ name: 'J.R.R. Tolkien' })
+    await tolkien.save()
+
+    const rowling = newAuthor({ name: 'J.K. Rowling' })
+    await rowling.save()
+    await new Book ({title: 'The Hobbit', author: tolkien}).save()
+  }
+  seedDatabase()
+ }
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,6 +48,16 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+})
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find().populate('author')
+  res.json(books)
 })
 
 // Start the server
